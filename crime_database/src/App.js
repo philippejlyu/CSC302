735b84d40abcfae67339732cbba7ee07d1895ee5
Piyush Sharma <piyush19sha@gmail.com>
2022-10-31 19:15:10 -0400

    Initial Commit

--- crime_database/src/App.js
+++ crime_database/src/App.js
@@ -1,38 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 import './SideBar.js';
-import Dropdown from 'react-bootstrap/Dropdown';
+import React from 'react';
+import SideBar from './SideBar.js';
 
 function App() {
   return (
-    <div className="App">
-      <div className="MapView">
-      <Dropdown.Menu show>
-                    <Dropdown.Header>My Datasets</Dropdown.Header>
-                    <Dropdown.Item eventKey="2">Dataset A</Dropdown.Item>
-                    <Dropdown.Item eventKey="3">Dataset B</Dropdown.Item>
-                </Dropdown.Menu>
-                <Dropdown.Menu show>
-                    <Dropdown.Header>Visualizations</Dropdown.Header>
-                    <Dropdown.Item eventKey="2">Crime heat map</Dropdown.Item>
-                    <Dropdown.Item eventKey="3">Crime vs Population</Dropdown.Item>
-                </Dropdown.Menu>
-      </div>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <React.Fragment>
+      <SideBar></SideBar>
+      </React.Fragment>
   );
 }
 
--- crime_database/src/SideBar.js
+++ crime_database/src/SideBar.js
@@ -1,25 +1,9 @@
-import React, { Component } from 'react';
-import Nav from 'react-bootstrap/Nav';
-import Dropdown from 'react-bootstrap/Dropdown';
-import Container from 'react-bootstrap/Container';
-import Navbar from 'react-bootstrap/Navbar'
-class SideBar extends Component {
-    render(){
-        return(
-            <>
-                <Dropdown.Menu show>
-                    <Dropdown.Header>My Datasets</Dropdown.Header>
-                    <Dropdown.Item eventKey="2">Dataset A</Dropdown.Item>
-                    <Dropdown.Item eventKey="3">Dataset B</Dropdown.Item>
-                </Dropdown.Menu>
-                <Dropdown.Menu show>
-                    <Dropdown.Header>Visualizations</Dropdown.Header>
-                    <Dropdown.Item eventKey="2">Crime heat map</Dropdown.Item>
-                    <Dropdown.Item eventKey="3">Crime vs Population</Dropdown.Item>
-                </Dropdown.Menu>
-            </>
-        )
-    }
+import React from 'react';
+
+const SideBar = () => {
+    return (
+        <h1>Hello</h1>
+    )
 }
 
 export default SideBar;
