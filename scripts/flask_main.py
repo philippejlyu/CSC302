18b72299ff925548cc5c1a60b7163cf763645f0e
Philippe Yu <philippe.yu@mail.utoronto.ca>
2022-10-31 21:52:53 -0400

    Docker will now compile webapp, serve backend

--- scripts/flask_main.py
+++ scripts/flask_main.py
@@ -1,10 +1,18 @@
-from flask import Flask
+import os
+from flask import Flask, send_from_directory
 
-app = Flask(__name__)
+# Debug:
+# app = Flask(__name__, static_folder='../crime_database/build')
+# Docker:
+app = Flask(__name__, static_folder='build')
 
-@app.route("/")
-def home():
-    return "Hello, World!"
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def serve(path):
+    if path != "" and os.path.exists(app.static_folder + '/' + path):
+        return send_from_directory(app.static_folder, path)
+    else:
+        return send_from_directory(app.static_folder, 'index.html')
     
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=3000)
