6ed60f18e23617b03cf900be9c0d556ca601d1cd
Philippe Yu <philippe.yu@mail.utoronto.ca>
2022-11-03 19:10:37 -0400

    Loaded all local data onto the map

--- crime_database/src/Map.js
+++ crime_database/src/Map.js
@@ -6,6 +6,7 @@ import 'leaflet/dist/leaflet.css';
 import icon from 'leaflet/dist/images/marker-icon.png';
 import iconShadow from 'leaflet/dist/images/marker-shadow.png';
 import React from 'react';
+import mapData from './local_data.json';
 
 let DefaultIcon = L.icon({
     iconUrl: icon,
@@ -19,43 +20,30 @@ class Map extends React.Component {
 
 
     componentDidMount() {
-        // TODO: Take locations from rest api
-        const locations = [{
-            "lat": 43.83892,
-            "lon": -79.38338,
-            "name": "Qualcomm"
-        },
-        {
-            "lat": 43.83932307332953,
-            "lon": -79.37992873760615,
-            "name": "AMD"
-        },
-        {
-            "lat": 43.84047298186827,
-            "lon": -79.37932548774376,
-            "name": "CAA Travel"
-        },
-        {
-            "lat": 43.84181967101403,
-            "lon": -79.38076178308928,
-            "name": "Mariott 1"
-        },
-        {
-            "lat": 43.84195435370095,
-            "lon": -79.37956967370914,
-            "name": "Mariott 2"
-        },
-        {
-            "lat": 43.84170561832021,
-            "lon": -79.37375274146041,
-            "name": "Lisa's Cosmetics"
-        }]
+        var locations = []
+        for (let i = 0; i < mapData.rows.length; i++) {
+            locations.push({
+                "lat": mapData.rows[i][146],
+                "lon": mapData.rows[i][147],
+                "cityname": mapData.rows[i][0],
+                "population": mapData.rows[i][4],
+                "populationDensity": mapData.rows[i][120],
+                "murders": mapData.rows[i][128],
+                "robberies": mapData.rows[i][132],
+                "assaults": mapData.rows[i][134],
+                "burglaries": mapData.rows[i][136],
+                "larcenies": mapData.rows[i][138],
+                "autoTheft": mapData.rows[i][140],
+                "arson": mapData.rows[i][142],
+            })
+        }
+        console.log(mapData);
         this.setState({markers: locations});
     }
 
     render () {
         return (
-            <MapContainer center={[43.83878, -79.37967]} zoom={13} scrollWheelZoom={false}>
+            <MapContainer center={[40.762730914502896, -73.97376139655083]} zoom={10} scrollWheelZoom={false}>
             <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -65,7 +53,16 @@ class Map extends React.Component {
 
                     return(<Marker position={[location.lat, location.lon]}>
                         <Popup>
-                        {location.name}
+                        <center><b>{location.cityname}</b></center><br></br>
+                        Population: {location.population}<br></br>
+                        Population Density: {location.populationDensity}<br></br>
+                        Murders: {location.murders}<br></br>
+                        Robberies: {location.robberies}<br></br>
+                        Assaults: {location.assaults}<br></br>
+                        Burglaries: {location.burglaries}<br></br>
+                        Larcenies: {location.larcenies}<br></br>
+                        Auto Theft: {location.autoTheft}<br></br>
+                        Arsons: {location.arson}
                         </Popup>
                     </Marker>)
             })}
