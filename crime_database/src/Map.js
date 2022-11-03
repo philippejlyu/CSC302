13acee8f89e538a561c16a640c40b99405b3df8d
Philippe Yu <philippe.yu@mail.utoronto.ca>
2022-11-03 12:03:41 -0400

    Basic map visualization

---
+++ crime_database/src/Map.js
@@ -0,0 +1,32 @@
+import L from 'leaflet';
+import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet'
+import './App.css'
+import 'leaflet/dist/leaflet.css';
+
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+
+let DefaultIcon = L.icon({
+    iconUrl: icon,
+    shadowUrl: iconShadow
+});
+
+L.Marker.prototype.options.icon = DefaultIcon;
+
+function Map() {
+  return (
+    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+    <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    />
+    <Marker position={[51.505, -0.09]}>
+    <Popup>
+      A pretty CSS3 popup. <br /> Easily customizable.
+    </Popup>
+  </Marker>
+    </MapContainer>
+  );
+}
+
+export default Map;
