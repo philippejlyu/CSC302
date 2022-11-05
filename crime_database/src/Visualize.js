9fb0ecca4614817ac525bf76f2d160bc678046dc
Piyush Sharma <piyush19sha@gmail.com>
2022-11-04 22:06:56 -0400

    Added Philippe's Map Component to Visualize Page

--- crime_database/src/Visualize.js
+++ crime_database/src/Visualize.js
@@ -1,5 +1,6 @@
 import React from "react";
 import SideBar from "./SideBar";
+import Map from "./Map";
 
 const Visualize = () => {
 
@@ -8,6 +9,7 @@ const Visualize = () => {
         <SideBar></SideBar>
         <div id='main-page' style={{marginTop:'75px', marginLeft: '300px'}}>
         <h1>Visualize</h1>
+        <Map></Map>
       </div>
       </React.Fragment>
     )
