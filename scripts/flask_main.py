e8c8deaf05b08d89a3b201a8ad4f8ac8697fe975
Philippe Yu <philippe.yu@mail.utoronto.ca>
2022-11-04 21:44:02 -0400

    Serve json file from server

--- scripts/flask_main.py
+++ scripts/flask_main.py
@@ -1,10 +1,21 @@
 import os
 from flask import Flask, send_from_directory
+import json
 
 # Debug:
 # app = Flask(__name__, static_folder='../crime_database/build')
 # Docker:
-app = Flask(__name__, static_folder='build')
+app = Flask(__name__, static_folder='')
+
+@app.route('/mapData', methods=['GET'])
+def getMapData():
+    print(app.static_folder)
+
+    json_file_path = app.static_folder + '/src/local_data.json'
+
+    with open(json_file_path, 'r') as j:
+        contents = json.loads(j.read())
+        return contents
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
