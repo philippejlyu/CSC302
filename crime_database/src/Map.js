27e04bfc4be4721763e931cd2ec7531d4d40cab1
Philippe Yu <philippe.yu@mail.utoronto.ca>
2022-12-01 23:47:01 -0500

    Changes to support new backend design

--- crime_database/src/Map.js
+++ crime_database/src/Map.js
@@ -18,6 +18,9 @@ L.Marker.prototype.options.icon = DefaultIcon;
 const Map = () => {
     const [loaded, setLoaded] = useState(false);
     const [markers, setMarkers] = useState(null);
+    const [stateMarkers, setStateMarkers] = useState(null);
+    // TODO: Mack will pass in dataset name here as a prop
+    const dataset = "crime.db";
     var zoomLevel = 10;
 
     const MapControllerComponent = () => {
@@ -30,33 +33,64 @@ const Map = () => {
         return null
       }
     React.useEffect(() => {
-        fetch('http://localhost:3000/mapData')
+        fetch('http://localhost:3000/mapData?datasetID=' + dataset)
         .then(function(res) {
             if (res.status === 200) {
                 return res.json();
             }
         }).then(mapData => {
             var locations = []
+            console.log(mapData);
             for (var i = 0; i < mapData.rows.length; i++) {
+                // console.log("Locations: " + mapData.rows[i].latitude + ", " + mapData.rows[i].longitude);
                 locations.push({
-                    "lat": mapData.rows[i][148],
-                    "lon": mapData.rows[i][149],
-                    "cityname": mapData.rows[i][1],
-                    "population": mapData.rows[i][5],
-                    "populationDensity": mapData.rows[i][121],
-                    "murders": mapData.rows[i][129],
-                    "robberies": mapData.rows[i][133],
-                    "assaults": mapData.rows[i][135],
-                    "burglaries": mapData.rows[i][137],
-                    "larcenies": mapData.rows[i][139],
-                    "autoTheft": mapData.rows[i][141],
-                    "arson": mapData.rows[i][143],
-                    "boundingBox": mapData.rows[i][147]
+                    "lat": mapData.rows[i][147],
+                    "lon": mapData.rows[i][148],
+                    "cityname": mapData.rows[i][0],
+                    "population": mapData.rows[i][4],
+                    "populationDensity": mapData.rows[i][120],
+                    "murders": mapData.rows[i][128],
+                    "robberies": mapData.rows[i][132],
+                    "assaults": mapData.rows[i][134],
+                    "burglaries": mapData.rows[i][136],
+                    "larcenies": mapData.rows[i][138],
+                    "autoTheft": mapData.rows[i][140],
+                    "arson": mapData.rows[i][142],
+                    "boundingBox": mapData.rows[i][146]
                 })
             }
+            console.log('markers set')
             setMarkers(locations);
             setLoaded(true);
         });
+
+        // fetch('http://localhost:3000/mapData?stateLevel&datasetID=' + dataset)
+        // .then(function(res) {
+        //     if (res.status === 200) {
+        //         return res.json();
+        //     }
+        // }).then(mapData => {
+        //     var locations = []
+        //     for (var i = 0; i < mapData.rows.length; i++) {
+        //         locations.push({
+        //             "lat": mapData.rows[i][148],
+        //             "lon": mapData.rows[i][149],
+        //             "cityname": mapData.rows[i][1],
+        //             "population": mapData.rows[i][5],
+        //             "populationDensity": mapData.rows[i][121],
+        //             "murders": mapData.rows[i][129],
+        //             "robberies": mapData.rows[i][133],
+        //             "assaults": mapData.rows[i][135],
+        //             "burglaries": mapData.rows[i][137],
+        //             "larcenies": mapData.rows[i][139],
+        //             "autoTheft": mapData.rows[i][141],
+        //             "arson": mapData.rows[i][143],
+        //             "boundingBox": mapData.rows[i][147]
+        //         })
+        //     }
+        //     setStateMarkers(locations);
+        //     setLoaded(true);
+        // });
     }, []);
 
     return (
