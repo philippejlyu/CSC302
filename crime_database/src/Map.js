3094fca4d3216fafd388e2d014792754eb22a8b5
Philippe Yu <philippe.yu@mail.utoronto.ca>
2022-12-01 23:09:14 -0500

    Convert maps to use react hooks

--- crime_database/src/Map.js
+++ crime_database/src/Map.js
@@ -1,11 +1,11 @@
 import L from 'leaflet';
-import { MapContainer, TileLayer, useMap, Marker, Popup, Polygon } from 'react-leaflet'
+import { MapContainer, TileLayer, useMap, useMapEvents, Marker, Popup, Polygon } from 'react-leaflet'
 import './App.css'
 import 'leaflet/dist/leaflet.css';
 
 import icon from 'leaflet/dist/images/marker-icon.png';
 import iconShadow from 'leaflet/dist/images/marker-shadow.png';
-import React from 'react';
+import React, { useState } from 'react';
 
 let DefaultIcon = L.icon({
     iconUrl: icon,
@@ -14,24 +14,30 @@ let DefaultIcon = L.icon({
 
 L.Marker.prototype.options.icon = DefaultIcon;
 
-class Map extends React.Component {
-    state = { markers: [] };
 
+const Map = () => {
+    const [loaded, setLoaded] = useState(false);
+    const [markers, setMarkers] = useState(null);
+    var zoomLevel = 10;
 
-    componentDidMount() {
-
-        fetch('http://localhost:3000/mapData')
-        .then(function (res) {
-            console.log(res);
-            if (res.status === 200) {
-                console.log('res 200')
-                return res.json()
-            }
+    const MapControllerComponent = () => {
+        const map = useMapEvents({
+        zoomend: () => {
+            zoomLevel = map.getZoom();
+            console.log(zoomLevel);
+        },
         })
-        .then(mapData => {
+        return null
+      }
+    React.useEffect(() => {
+        fetch('http://localhost:3000/mapData')
+        .then(function(res) {
+            if (res.status === 200) {
+                return res.json();
+            }
+        }).then(mapData => {
             var locations = []
-            for (let i = 0; i < 400; i++) {
-                // console.log(mapData.rows[i][147])
+            for (var i = 0; i < mapData.rows.length; i++) {
                 locations.push({
                     "lat": mapData.rows[i][148],
                     "lon": mapData.rows[i][149],
@@ -47,50 +53,47 @@ class Map extends React.Component {
                     "arson": mapData.rows[i][143],
                     "boundingBox": mapData.rows[i][147]
                 })
-        }
-        this.setState({markers: locations});
-        })
-        .catch(error => {
-            console.log(error);
-        })
-    }
+            }
+            setMarkers(locations);
+            setLoaded(true);
+        });
+    }, []);
 
-    render () {
-        return (
-            <MapContainer center={[40.762730914502896, -73.97376139655083]} zoom={10} scrollWheelZoom={false}>
-            <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            {this.state.markers.length > 0 && 
-                this.state.markers.map((location) => {
-                    const purpleOptions = { colour: 'purple' }
+    return (
+        <MapContainer center={[40.762730914502896, -73.97376139655083]} zoom={10} scrollWheelZoom={false}>
+            <MapControllerComponent />
+        <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {loaded && markers.length > 0 && 
+            markers.map((location) => {
+                const purpleOptions = { colour: 'purple' }
 
-                    const polygon = JSON.parse(location.boundingBox);
-                    
-                    // console.log(polygon)
-                    
-                    return(<Polygon pathOptions={purpleOptions} positions={polygon} key={location.cityname}>
-                        <Popup>
-                         <center><b>{location.cityname}</b></center><br></br>
-                         Population: {location.population}<br></br>
-                         Population Density: {location.populationDensity}<br></br>
-                         Murders: {location.murders}<br></br>
-                         Robberies: {location.robberies}<br></br>
-                         Assaults: {location.assaults}<br></br>
-                         Burglaries: {location.burglaries}<br></br>
-                         Larcenies: {location.larcenies}<br></br>
-                         Auto Theft: {location.autoTheft}<br></br>
-                         Arsons: {location.arson}
-                         </Popup>
+                const polygon = JSON.parse(location.boundingBox);
+                
+                // console.log(polygon)
+                
+                return(<Polygon pathOptions={purpleOptions} positions={polygon} key={location.cityname} className="statePolygon">
+                    <Popup>
+                     <center><b>{location.cityname}</b></center><br></br>
+                     Population: {location.population}<br></br>
+                     Population Density: {location.populationDensity}<br></br>
+                     Murders: {location.murders}<br></br>
+                     Robberies: {location.robberies}<br></br>
+                     Assaults: {location.assaults}<br></br>
+                     Burglaries: {location.burglaries}<br></br>
+                     Larcenies: {location.larcenies}<br></br>
+                     Auto Theft: {location.autoTheft}<br></br>
+                     Arsons: {location.arson}
+                     </Popup>
 
-                    </Polygon>)
-                    
-            })}
-            
-            </MapContainer>
-          );
-    }
+                </Polygon>)
+                
+        })}
+        
+        </MapContainer>
+      );
 }
 
 export default Map;
