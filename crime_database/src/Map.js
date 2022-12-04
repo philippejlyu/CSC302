e43f6e59b11ef89745e8ec6cc7ab9da08055bc3c
Samaria Mulligan <mulliganaceous@outlook.com>
2022-12-04 18:58:53 -0500

    Completed select dataset. All uploads are sent to the db folder inside the backend.
    Some adhoc solutions are still made

--- crime_database/src/Map.js
+++ crime_database/src/Map.js
@@ -13,29 +13,84 @@ let DefaultIcon = L.icon({
 });
 
 L.Marker.prototype.options.icon = DefaultIcon;
-
+const sample = "sample.db";
 
 const Map = () => {
     const [loaded, setLoaded] = useState(false);
     const [markers, setMarkers] = useState(null);
     const [stateMarkers, setStateMarkers] = useState(null);
     const [showStates, setShowStates] = useState(false);
-    // TODO: Mack will pass in dataset name here as a prop
-    const dataset = "sample.db";
+    const [dataset, setDataset] = useState(sample);
+
     var zoomLevel = 10;
+
+    const f = "g";
 
     const MapControllerComponent = () => {
 
         const loadStateMarkers = () => {
+            console.log("Map: Loaded state markers on demand");
             fetch('http://localhost:3000/mapData?stateLevel&datasetID=' + dataset)
-            .then(function(res) {
+                .then(function (res) {
+                    if (res.status === 200) {
+                        return res.json();
+                    }
+                }).then(mapData => {
+                    var stateLocations = []
+                    for (var i = 0; i < mapData.rows.length; i++) {
+                        stateLocations.push({
+                            "lat": mapData.rows[i][147],
+                            "lon": mapData.rows[i][148],
+                            "cityname": mapData.rows[i][0],
+                            "population": mapData.rows[i][4],
+                            "populationDensity": mapData.rows[i][120],
+                            "murders": mapData.rows[i][128],
+                            "robberies": mapData.rows[i][132],
+                            "assaults": mapData.rows[i][134],
+                            "burglaries": mapData.rows[i][136],
+                            "larcenies": mapData.rows[i][138],
+                            "autoTheft": mapData.rows[i][140],
+                            "arson": mapData.rows[i][142],
+                            "boundingBox": mapData.rows[i][146],
+                            "id": "stateLevel" + i
+                        })
+                    }
+                    setStateMarkers(stateLocations);
+                    // setLoaded(true);
+                });
+        }
+
+        const map = useMapEvents({
+            zoomend: () => {
+                if (stateMarkers == null) {
+                    loadStateMarkers();
+                }
+                // Toggle when zoom crosses level 8
+                if (map.getZoom() < 8 && !showStates) {
+                    setShowStates(true);
+                } else if (map.getZoom() >= 8 && showStates) {
+                    setShowStates(false);
+                }
+            },
+        })
+        
+        return null
+    }
+
+    React.useEffect(() => {
+        console.log("Map use Effect");
+        const fetchURL = 'http://localhost:3000/mapData?datasetID=' + dataset;
+        fetch(fetchURL)
+            .then(function (res) {
                 if (res.status === 200) {
                     return res.json();
                 }
             }).then(mapData => {
-                var stateLocations = []
+                var locations = []
+                console.log(mapData);
                 for (var i = 0; i < mapData.rows.length; i++) {
-                    stateLocations.push({
+                    // console.log("Locations: " + mapData.rows[i].latitude + ", " + mapData.rows[i].longitude);
+                    locations.push({
                         "lat": mapData.rows[i][147],
                         "lon": mapData.rows[i][148],
                         "cityname": mapData.rows[i][0],
@@ -49,145 +104,96 @@ const Map = () => {
                         "autoTheft": mapData.rows[i][140],
                         "arson": mapData.rows[i][142],
                         "boundingBox": mapData.rows[i][146],
-                        "id": "stateLevel" + i
+                        "id": i
                     })
                 }
-                setStateMarkers(stateLocations);
-                // setLoaded(true);
+                setMarkers(locations);
+                setLoaded(true);
+                setDataset(dataset);
             });
-        }
-
-
-        const map = useMapEvents({
-        zoomend: () => {
-            if (stateMarkers == null) {
-                loadStateMarkers();
-            }
-            // Toggle when zoom crosses level 8
-            if (map.getZoom() < 8 && !showStates) {
-                setShowStates(true);
-            } else if (map.getZoom() >= 8 && showStates) {
-                setShowStates(false);
-            }
-        },
-        })
-        return null
-      }
-    React.useEffect(() => {
-        const fetchURL = 'http://localhost:3000/mapData?datasetID=' + dataset;
-        fetch(fetchURL)
-        .then(function(res) {
-            if (res.status === 200) {
-                return res.json();
-            }
-        }).then(mapData => {
-            var locations = []
-            console.log(mapData);
-            for (var i = 0; i < mapData.rows.length; i++) {
-                // console.log("Locations: " + mapData.rows[i].latitude + ", " + mapData.rows[i].longitude);
-                locations.push({
-                    "lat": mapData.rows[i][147],
-                    "lon": mapData.rows[i][148],
-                    "cityname": mapData.rows[i][0],
-                    "population": mapData.rows[i][4],
-                    "populationDensity": mapData.rows[i][120],
-                    "murders": mapData.rows[i][128],
-                    "robberies": mapData.rows[i][132],
-                    "assaults": mapData.rows[i][134],
-                    "burglaries": mapData.rows[i][136],
-                    "larcenies": mapData.rows[i][138],
-                    "autoTheft": mapData.rows[i][140],
-                    "arson": mapData.rows[i][142],
-                    "boundingBox": mapData.rows[i][146],
-                    "id": i
-                })
-            }
-            setMarkers(locations);
-            setLoaded(true);
-        });
     }, []);
 
     return (
         <MapContainer center={[40.762730914502896, -73.97376139655083]} zoom={10} scrollWheelZoom={true}>
             <MapControllerComponent />
-        <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        {/* City data */}
-        {loaded && !showStates && markers.length > 0 && 
-            markers.map((location) => {
-                let violentPerPop = (location.murders + location.assaults)/location.population;
-                var purpleOptions = {}
-                if(violentPerPop < 0.003){
-                    purpleOptions = { fillColor: 'green', color:'green' }
-                }
-                else if(violentPerPop >= 0.003 && violentPerPop < 0.005){
-                    purpleOptions = { fillColor: 'yellow', color: '#fcdb03'}
-                }
-                else if(violentPerPop >= 0.005 && violentPerPop < 0.007){
-                    purpleOptions = { fillColor: 'orange', color: 'orange' }
-                }
-                else if(violentPerPop >= 0.007){
-                    purpleOptions = { fillColor: 'red', color: 'red' }
-                }
+            <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            {/* City data */}
+            {loaded && !showStates && markers.length > 0 &&
+                markers.map((location) => {
+                    let violentPerPop = (location.murders + location.assaults) / location.population;
+                    var purpleOptions = {}
+                    if (violentPerPop < 0.003) {
+                        purpleOptions = { fillColor: 'green', color: 'green' }
+                    }
+                    else if (violentPerPop >= 0.003 && violentPerPop < 0.005) {
+                        purpleOptions = { fillColor: 'yellow', color: '#fcdb03' }
+                    }
+                    else if (violentPerPop >= 0.005 && violentPerPop < 0.007) {
+                        purpleOptions = { fillColor: 'orange', color: 'orange' }
+                    }
+                    else if (violentPerPop >= 0.007) {
+                        purpleOptions = { fillColor: 'red', color: 'red' }
+                    }
 
-                const polygon = JSON.parse(location.boundingBox);
-                
-                
-                return(<Polygon pathOptions={purpleOptions} positions={polygon} key={location.id}>
-                    <Popup>
-                     <center><b>{location.cityname}</b></center><br></br>
-                     Population: {location.population}<br></br>
-                     Population Density: {location.populationDensity}<br></br>
-                     Murders: {location.murders}<br></br>
-                     Robberies: {location.robberies}<br></br>
-                     Assaults: {location.assaults}<br></br>
-                     Burglaries: {location.burglaries}<br></br>
-                     Larcenies: {location.larcenies}<br></br>
-                     Auto Theft: {location.autoTheft}<br></br>
-                     Arsons: {location.arson}
-                     </Popup>
+                    const polygon = JSON.parse(location.boundingBox);
 
-                </Polygon>)
-                
-        })}
-        {/* State data */}
-        {loaded && showStates && stateMarkers.length > 0 && 
-            stateMarkers.map((location) => {
-                let violentPerPop = (location.murders + location.assaults)/location.population;
-                var purpleOptions = {}
-                if(violentPerPop < 0.003){
-                    purpleOptions = { fillColor: 'green', color:'green' }
-                }
-                else if(violentPerPop >= 0.003 && violentPerPop < 0.005){
-                    purpleOptions = { fillColor: 'yellow', color: '#fcdb03'}
-                }
-                else if(violentPerPop >= 0.005 && violentPerPop < 0.007){
-                    purpleOptions = { fillColor: 'orange', color: 'orange' }
-                }
-                else if(violentPerPop >= 0.007){
-                    purpleOptions = { fillColor: 'red', color: 'red' }
-                }
-                const polygon = JSON.parse(location.boundingBox);
-                                
-                return(<Polygon pathOptions={purpleOptions} positions={polygon} key={location.id}>
-                    <Popup>
-                     <center><b>{location.cityname}</b></center><br></br>
-                     Population: {location.population}<br></br>
-                     Population Density: {location.populationDensity}<br></br>
-                     Murders: {location.murders}<br></br>
-                     Robberies: {location.robberies}<br></br>
-                     Assaults: {location.assaults}<br></br>
-                     Burglaries: {location.burglaries}<br></br>
-                     Larcenies: {location.larcenies}<br></br>
-                     Auto Theft: {location.autoTheft}<br></br>
-                     Arsons: {location.arson}
-                     </Popup>
-                </Polygon>)
-        })}
+
+                    return (<Polygon pathOptions={purpleOptions} positions={polygon} key={location.id}>
+                        <Popup>
+                            <center><b>{location.cityname}</b></center><br></br>
+                            Population: {location.population}<br></br>
+                            Population Density: {location.populationDensity}<br></br>
+                            Murders: {location.murders}<br></br>
+                            Robberies: {location.robberies}<br></br>
+                            Assaults: {location.assaults}<br></br>
+                            Burglaries: {location.burglaries}<br></br>
+                            Larcenies: {location.larcenies}<br></br>
+                            Auto Theft: {location.autoTheft}<br></br>
+                            Arsons: {location.arson}
+                        </Popup>
+
+                    </Polygon>)
+
+                })}
+            {/* State data */}
+            {loaded && showStates && stateMarkers.length > 0 &&
+                stateMarkers.map((location) => {
+                    let violentPerPop = (location.murders + location.assaults) / location.population;
+                    var purpleOptions = {}
+                    if (violentPerPop < 0.003) {
+                        purpleOptions = { fillColor: 'green', color: 'green' }
+                    }
+                    else if (violentPerPop >= 0.003 && violentPerPop < 0.005) {
+                        purpleOptions = { fillColor: 'yellow', color: '#fcdb03' }
+                    }
+                    else if (violentPerPop >= 0.005 && violentPerPop < 0.007) {
+                        purpleOptions = { fillColor: 'orange', color: 'orange' }
+                    }
+                    else if (violentPerPop >= 0.007) {
+                        purpleOptions = { fillColor: 'red', color: 'red' }
+                    }
+                    const polygon = JSON.parse(location.boundingBox);
+
+                    return (<Polygon pathOptions={purpleOptions} positions={polygon} key={location.id}>
+                        <Popup>
+                            <center><b>{location.cityname}</b></center><br></br>
+                            Population: {location.population}<br></br>
+                            Population Density: {location.populationDensity}<br></br>
+                            Murders: {location.murders}<br></br>
+                            Robberies: {location.robberies}<br></br>
+                            Assaults: {location.assaults}<br></br>
+                            Burglaries: {location.burglaries}<br></br>
+                            Larcenies: {location.larcenies}<br></br>
+                            Auto Theft: {location.autoTheft}<br></br>
+                            Arsons: {location.arson}
+                        </Popup>
+                    </Polygon>)
+                })}
         </MapContainer>
-      );
+    );
 }
 
 export default Map;
--- crime_database/src/Visualize.js
+++ crime_database/src/Visualize.js
@@ -1,33 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 import SideBar from "./SideBar";
 import Map from "./Map";
 import FormControl from "@mui/material/FormControl";
 import InputLabel from "@mui/material/InputLabel";
 import Select from "@mui/material/Select";
 import MenuItem from "@mui/material/MenuItem";
+import { SERVERSIDEPORT } from './App.js';
+
+var rows = []
+var map = <Map id={"map"} data={"sample.db"}></Map>
+
+const fetchDBFiles = () => {
+  fetch('http://localhost:' + SERVERSIDEPORT + '/mapData')
+  .then(function (res) {
+    console.log(res);
+    if (res.status === 200) {
+      console.log('200: Visualization');
+      return res.json(); // Becomes the map data
+    }
+  })
+  .then(res => {
+    rows = [""];
+    for (var k = 0; k < res.length; k++) {
+      rows.push(res[k]);
+    }
+  })
+  .catch(error => {
+    console.log(error);
+  })
+}
 
 const Visualize = () => {
-  var age = 1;
+  const [dataset, setDataset] = useState("sample.db");
+  fetchDBFiles();
+  console.info("Visualize rerender");
   return (
     <React.Fragment>
       <SideBar></SideBar>
       <div id='main-page' style={{ marginTop: '65px', marginLeft: '275px' }}>
-        
         <FormControl fullWidth>
-          <InputLabel id="demo-simple-select-label">Age</InputLabel>
+          <InputLabel id="demo-simple-select-label">Select Dataset</InputLabel>
           <Select
             labelId="demo-simple-select-label"
             id="demo-simple-select"
-            value={age}
-            label="Age"
-            onChange={()=>{}}
+            value={dataset}
+            label="Dataset Selection"
+            onChange={(event)=>{setDataset(event.target.value); console.info(dataset); map=null; map=<Map id={"map"} data={"sample.db"}></Map>}}
           >
-            <MenuItem value={10}>Ten</MenuItem>
-            <MenuItem value={20}>Twenty</MenuItem>
-            <MenuItem value={30}>Thirty</MenuItem>
+          {console.log(rows)}
+          {rows.map((name) => (
+            <MenuItem
+              key={name}
+              value={name}
+            >
+              {"* " + name}
+            </MenuItem>
+          ))}
           </Select>
         </FormControl>
-        <Map></Map>
+        {map}
       </div>
     </React.Fragment>
   )
